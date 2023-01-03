49f7bf7b524829654698e5a9602f94a21e29aac9
Priyatamkumar <93855467+Priyatamkumar@users.noreply.github.com>
2023-01-03 14:40:44 +0530

    Update 581_Shortest-Unsorted-Continuous-Subarray.cpp

--- 581_Shortest-Unsorted-Continuous-Subarray.cpp
+++ 581_Shortest-Unsorted-Continuous-Subarray.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     int findUnsortedSubarray(vector<int>& nums) {
+        
+        //method one
+        
+        
         vector<int>v;
         v=nums;
         sort(v.begin(),v.end());
@@ -17,5 +21,69 @@ public:
             j--;
         }
         return (j-i+1)>=0?(j-i+1):0;
+
+        
+        
+        
+        
+        //method 2 using stack
+        
+        
+        
+        stack<int>st;
+        int n=nums.size();
+        int l=n;
+        st.push(0);
+        for(int i=1;i<n;i++)
+        {
+            if(nums[i]>=nums[i-1])
+            {
+                st.push(i);
+            }
+            else
+            {
+                while(!st.empty()&&nums[st.top()]>nums[i])
+                {
+                    st.pop();
+                    if(st.empty())
+                    {
+                        l=-1;
+                        break;
+                    }
+                    else
+                        l=min(l,st.top());
+                }
+                st.push(i);
+            }
+        }
+        if(l==n)
+            return 0;
+        while(!st.empty())
+            st.pop();
+        int h=0;
+        st.push(n-1);
+        for(int i=n-2;i>=0;i--)
+        {
+            if(nums[i+1]>=nums[i])
+            {
+                st.push(i);
+            }
+            else
+            {
+                while(!st.empty()&&nums[st.top()]<nums[i])
+                {
+                    st.pop();
+                    if(st.empty())
+                    {
+                        h=n;
+                        break;
+                    }
+                    else
+                        h=max(st.top(),h);
+                }
+                st.push(i);
+            }
+        }
+        return h-l>0?h-l-1:0;
     }
 };
